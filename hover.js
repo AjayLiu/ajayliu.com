d300d29fc9c3a5749be21bd899210b3e8283b5de
AjayLiu <ajayliu2928@gmail.com>
2020-06-17 17:23:11 -0700

    big remodel!

--- hover.js
+++ hover.js
@@ -1,5 +1,12 @@
-// $('.gametext').hover(function() { 
-//     $('.gametitle').fadeIn(); 
-// }, function() { 
-//     $('.gametitle').fadeOut(); 
-// });
+// var gametexts = document.getElementsByClassName("gametext");
+
+// var i;
+// for(i = 0; i < gametexts.length; i++){
+//     gametexts[i].classList.toggle('fadeIn');
+// }
+
+$(".gameBlock").hover(function(){
+    $(this).find(".gametext").css({"opacity": "1", "transition": "opacity 0.2s"});
+}, function(){
+    $(this).find(".gametext").css({"opacity": "0", "transition": "opacity 0.2s"});
+});
