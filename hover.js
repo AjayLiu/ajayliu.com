8f84cd0174ba583698d309b3096c5d53658b790c
AjayLiu <ajayliu2928@gmail.com>
2020-06-18 13:25:43 -0700

    better gallery detection

--- hover.js
+++ hover.js
@@ -2,10 +2,52 @@ gallery();
 
 function gallery (){
     if(window.innerWidth > 600){
-        $(".gameBlock").mouseenter(function(){
-            $(this).find(".gametext").css({"opacity": "1", "transition": "opacity 0.2s"});
-        }).mouseleave(function(){
-            $(this).find(".gametext").css({"opacity": "0", "transition": "opacity 0.2s"});
+        
+        //Track mouse pos
+        var x, y;
+        $(document).mousemove(function(event){
+            x = event.clientX;
+            y = event.clientY;
+        });
+
+
+        var displayedDiv = null;
+        window.onscroll = function(){
+            var divs = document.elementsFromPoint(x, y);
+            if(divs != null){
+                var i;
+                var found = false;
+                for(i = 0; i < divs.length; i++){                    
+                    if(divs[i].className == "gameBlock"){
+                        if(displayedDiv == null || divs[i].children[i] != displayedDiv){
+                            if(displayedDiv != null){
+                                //old div
+                                displayedDiv.style.opacity = "0";
+                                displayedDiv.style.transition = "opacity 0.5s";
+                            }
+
+                            //new div
+                            displayedDiv = divs[i].children[0];
+                            displayedDiv.style.opacity = "1";
+                            displayedDiv.style.transition = "opacity 0.1s";
+                            found = true;
+                            break;
+                        }
+                    }
+                }                
+                if(!found && displayedDiv != null){
+                    displayedDiv.style.opacity = "0";
+                    displayedDiv.style.transition = "opacity 0.5s";
+                }
+            }
+        }
+
+        
+
+        $(".gameBlock").mouseover(function(){
+            $(this).find(".gametext").css({"opacity": "1", "transition": "opacity 0.1s"});
+        }).mouseout(function(){
+            $(this).find(".gametext").css({"opacity": "0", "transition": "opacity 0.5s"});
         });
     } else {
         var gametexts = document.getElementsByClassName("gametext");    
@@ -26,3 +68,7 @@ window.onresize = function(event) {
     gallery();
 
 };
+
+jQuery(document.links).filter(function() { 
+    return this.hostname != window.location.hostname;   
+}).attr('target', '_blank');  
