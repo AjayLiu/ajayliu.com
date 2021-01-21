7beda3b89af18983fee6003ebf1b948b223f1f6b
AjayLiu <ajayliu2928@gmail.com>
2021-01-20 21:38:27 -0800

    better formatted form

--- components/Contact.js
+++ components/Contact.js
@@ -98,21 +98,22 @@ export default function Contact({email}) {
 
         <form className={styles.form} name="contact">
           <div className={styles.formTitle}>Contact Form</div>
-          <div className={styles.row}>
+          <div className={styles.fieldsContainer}>
+            <div className={styles.row}>
+              <p onChange={onChange} className={styles.field}>
+                <input className={styles.smallInput} type="text" name="name" placeholder="Your Name"/>
+              </p>
+              <p onChange={onChange} className={styles.field} id={styles.email}>
+                <input className={styles.smallInput} type="text" name="email" placeholder="Your Email"/>
+              </p>
+            </div>
             <p onChange={onChange} className={styles.field}>
-              <input className={styles.smallInput} type="text" name="name" placeholder="Your Name"/>
-            </p>
-            <p onChange={onChange} className={styles.field}>
-              <input className={styles.smallInput} type="text" name="email" placeholder="Your Email"/>
+              <textarea className={styles.messageBox} type="text" name="message" placeholder="Your Message"></textarea>
             </p>
           </div>
-          <p onChange={onChange} className={styles.field}>
-            <textarea className={styles.messageBox} type="text" name="message" placeholder="Your Message"></textarea>
-          </p>
           <button className={styles.formButton} onClick={submitForm}>{formState.buttonText}</button>
         </form>
       </div>
-      
     </section>
   )
 }
