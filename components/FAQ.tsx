b49489c5722876c038ffc76ab2cabe65948a2b8b
AjayLiu <ajayliu2928@gmail.com>
2021-02-05 13:43:46 -0800

    changed faq a lil

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -63,8 +63,8 @@ const FAQ: React.FC = () => {
                 <AccordionItemPanel>
                     <p>
                         This website was originally coded from pure HTML, CSS, and JavaScript. Now I have refactored
-                        the code to React and Next.js. This site is hosted on Netlify and all the code to this website
-                        can be found on my <a href = "https://github.com/ajayliu">github</a>.
+                        the code to React and Next.js, written in TypeScript. This site is hosted on Netlify and all 
+                        the code to this website can be found on my <a href = "https://github.com/ajayliu">github</a>.
                     </p>
                 </AccordionItemPanel>
             </AccordionItem>
