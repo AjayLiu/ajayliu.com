aa3c520a62d556fe6872c0fd003995073466d2bb
AjayLiu <ajayliu2928@gmail.com>
2021-02-08 15:55:47 -0800

    configure blog

---
+++ content/pages/home.md
@@ -0,0 +1,4 @@
+---
+hero_title: Build a SaaS faster with React
+hero_description: Serverless SaaS is aiming to be the perfect starting point for your next React app to build full-stack SaaS applications. Visit serverless.page for more info.
+---
---
+++ pages/admin.tsx
@@ -0,0 +1,16 @@
+import dynamic from 'next/dynamic';
+import config from '../cms/config';
+
+const CMS = dynamic(
+  () =>
+    import('netlify-cms-app').then((cms) => {
+      cms.init({ config });
+    }),
+  { ssr: false, loading: () => <p>Loading...</p> }
+);
+
+const AdminPage: React.FC = () => {
+  return <CMS />;
+};
+
+export default AdminPage;
