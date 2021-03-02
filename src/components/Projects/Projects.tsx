9a75aec671c464222b1914c6267fa87616aa1d99
AjayLiu <ajayliu2928@gmail.com>
2021-03-01 17:05:44 -0800

    updated new inspiration jar and reordered projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -22,7 +22,7 @@ const Projects: React.FC = () => {
           />
           <ProjectItem 
             bgImg="imgs/ichiban.png"
-            gridRow="1/5"
+            gridRow="1/4"
             gridCol="4/6"
             title="Ichiban" 
             link = "https://ichiban.netlify.app/" 
@@ -30,67 +30,58 @@ const Projects: React.FC = () => {
             description = "An anime popularity guessing game!"
           />
           <ProjectItem 
-            bgImg="imgs/cashOutSkyView.png"
-            gridRow="10/12"
-            gridCol="4/6"
-            title="Cash Out" 
-            link = "games/cashout/" 
-            playstoreLink="https://play.google.com/store/apps/details?id=com.TroyGameDevelopment.CashOut"
-            description = "A fast paced police pursuit game."
+            bgImg="imgs/inspirationbanner.png"
+            gridRow="4/7"
+            gridCol="1/3"
+            title="Inspiration Jar" 
+            link = "https://inspirationjar.herokuapp.com" 
+            description = "Share words of encouragement to fellow humans around the world!"
+          />
+          <ProjectItem 
+            bgImg="imgs/whenbanner.jpg"
+            gridRow="4/7"
+            gridCol = "3/5"
+            title="when?"
+            link = "https://when.netlify.app/"
+            description = "Have a worldwide event? Share a single link to convert that meeting time to everyone's local time!"
           />
           <ProjectItem 
             bgImg="imgs/goatsheepbanner.png"
             gridRow="4/7"
-            gridCol="3/3"
+            gridCol="5"
             title="Sheep or Goat?" 
             link = "https://sheepgoat.herokuapp.com/" 
             description = "Can't tell apart a sheep from a goat? Let the power of machine learning assist you!"
           />
           <ProjectItem 
             bgImg="imgs/pixelbeats.png"
-            gridRow="4/8"
+            gridRow="7/9"
             gridCol="1/3"
             title="Pixel Beats" 
             link = "games/pixelbeats/" 
             description = "Are you an artist, musician, or dancer? Tap to the beat and be them all!"
           />
           <ProjectItem 
-            bgImg="imgs/whenbanner.jpg"
-            gridRow="7/10"
-            gridCol = "3/5"
-            title="when?"
-            link = "https://when.netlify.app/"
-            description = "Have a worldwide event? Share a single link to convert that meeting time to everyone's local time!"
-          />
-
-          <ProjectItem 
-            bgImg="imgs/inspirationcapture.png"
-            gridRow="7/10"
-            gridCol="5/6"
-            title="Inspiration Jar" 
-            link = "https://inspirationjar.netlify.app" 
-            description = "(no longer supported) A platform for users to submit encouraging messages and make the world a little happier!"
+            bgImg="imgs/cashOutSkyView.png"
+            gridRow="7/9"
+            gridCol="3/5"
+            title="Cash Out" 
+            link = "games/cashout/" 
+            playstoreLink="https://play.google.com/store/apps/details?id=com.TroyGameDevelopment.CashOut"
+            description = "A fast paced police pursuit game."
           />
           <ProjectItem 
             bgImg="imgs/bouncebanner.PNG"
-            gridRow="5/7"
-            gridCol="4/6"
+            gridRow="7/9"
+            gridCol="5"
             title="Bounce Architect" 
             link = "games/bouncearchitect/" 
             playstoreLink="https://play.google.com/store/apps/details?id=com.HackathonGroup.BounceArchitect"
             description = "What shape bounces best in all orientations? Sketch it out now! Made in under 12 hours."
           />
           <ProjectItem 
-            bgImg="imgs/timerbanner.PNG"
-            gridRow="10/12"
-            gridCol="3"
-            title="10 Second Challenge" 
-            link = "https://10seconds.netlify.app/" 
-            description = "How well can you count 10 seconds in your head?"
-          />
-          <ProjectItem 
             bgImg="imgs/TapDefenderBanner.PNG"
-            gridRow="8/10"
+            gridRow="9/11"
             gridCol="1"
             title="Tap Defender" 
             link = "games/tapdefender/"
@@ -100,8 +91,8 @@ const Projects: React.FC = () => {
           />
           <ProjectItem 
             bgImg="imgs/colorjumpwide.png"
-            gridRow="8/10"
-            gridCol="2/3"
+            gridRow="9/11"
+            gridCol="2"
             title="Color Jump" 
             link="games/colorjump"
             playstoreLink="https://play.google.com/store/apps/details?id=com.MerbolaGames.ColorJump" 
@@ -109,9 +100,17 @@ const Projects: React.FC = () => {
             hardToRead
           />
           <ProjectItem 
+            bgImg="imgs/timerbanner.PNG"
+            gridRow="9/11"
+            gridCol="3"
+            title="10 Second Challenge" 
+            link = "https://10seconds.netlify.app/" 
+            description = "How well can you count 10 seconds in your head?"
+          />
+          <ProjectItem 
             bgImg="imgs/triviadashbanner.svg"
-            gridRow="10/12"
-            gridCol="1/3"
+            gridRow="9/11"
+            gridCol="4"
             link="games/triviadash"
             title="Trivia Dash" 
             description = "(work in progress) You're late for school! Study up and avoid the obstacles!"
