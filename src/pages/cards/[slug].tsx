dfa8ee00f0d890473b6c0f7d670b6bb86b50c521
AjayLiu <ajayliu2928@gmail.com>
2021-03-17 20:27:29 -0700

    forgot cards slug page

--- src/pages/cards/[slug].tsx
+++ src/pages/cards/[slug].tsx
@@ -1,40 +1,40 @@
-import Card from '@components/Card'
-import {cardList} from "public/cards/CardList.js"
+import Card from "@components/Card/Card";
+import { cardList } from "public/cards/CardList.js";
 
 interface Props {
-    slug: string;
-    contentsJSON: string; 
+  slug: string;
+  contentsJSON: string;
 }
 
-const CardPage: React.FC<Props> = ({slug}) => {
-    const props = cardList.find(el => el.slug == slug);
-    return (
-        <div>
-            <pre>
-                <Card props = {props} />
-            </pre>
-        </div>
-    )
-}
+const CardPage: React.FC<Props> = ({ slug }) => {
+  const props = cardList.find((el) => el.slug == slug);
+  return (
+    <div>
+      <pre>
+        <Card props={props} />
+      </pre>
+    </div>
+  );
+};
 
-export const getStaticPaths = async() => {
-    const paths = cardList.map(page => ({
-        params: {
-            slug: page.slug,
-        }
-    }))
-    return {
-        paths,
-        fallback: false
-    }
-}
+export const getStaticPaths = async () => {
+  const paths = cardList.map((page) => ({
+    params: {
+      slug: page.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-export const getStaticProps = async ({params: {slug}}) => {
-    return {
-        props : {
-            slug,
-        }
-    }
-}
+export const getStaticProps = async ({ params: { slug } }) => {
+  return {
+    props: {
+      slug,
+    },
+  };
+};
 
 export default CardPage;
