deda89bd2353aa7b475a7fabbd3bee3114028168
AjayLiu <ajayliu2928@gmail.com>
2021-03-25 20:59:52 -0700

    new footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,26 +1,29 @@
+import React from "react";
 import styles from "./Footer.module.scss";
-import { imgPath } from "public/path";
 const Footer: React.FC = () => {
+  const imgPath = "https://ajayliu.com/imgs";
   return (
     <>
       <footer className={styles.footer}>
         <p>
-          © 2021 <a href="https://ajayliu.com">Ajay Liu</a>. All Rights Reserved
-          •
+          © {new Date().getFullYear()}{" "}
+          <a href="https://ajayliu.com">Ajay Liu</a>. All Rights Reserved •{" "}
           <a href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@ajayliu.com">
-            {" "}
             contact@ajayliu.com
           </a>
         </p>
         <div id={styles.socialLinks}>
+          <a href="https://ajayliu.com">
+            <img src={imgPath + "/web_icon.svg"} alt="website logo" />
+          </a>
           <a href="https://github.com/AjayLiu">
-            <img src={imgPath + "/github.png"} alt="github logo" />
+            <img src={imgPath + "/github.svg"} alt="github logo" />
           </a>
           <a href="https://www.linkedin.com/in/ajayliu/">
-            <img src={imgPath + "/linkedin.png"} alt="linkedin logo" />
+            <img src={imgPath + "/linkedin.svg"} alt="linkedin logo" />
           </a>
           <a href="https://www.youtube.com/channel/UClr6XCaguPeM0g7UL6Lvs3g">
-            <img src={imgPath + "/youtubedark.png"} alt="dark youtube logo" />
+            <img src={imgPath + "/youtube.svg"} alt="youtube logo" />
           </a>
         </div>
       </footer>
