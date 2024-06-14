e8d96b0ba40225a6b6fa67a38ae64d8cca4f1695
AjayLiu <ajayliu2928@gmail.com>
2024-06-14 12:14:39 -0600

    added ersp, updated resume, added collaborators

--- src/components/ProjectItem/ProjectItem.tsx
+++ src/components/ProjectItem/ProjectItem.tsx
@@ -3,65 +3,74 @@ import { Skill } from "public/SkillList";
 import styles from "./ProjectItem.module.scss";
 
 interface Props {
-  title: string;
-  description: string;
-  image: string;
-  link?: string;
-  githubLink?: string;
-  playstoreLink?: string;
-  youtubeLink?: string;
-  devpostLink?: string;
-  madeWith: Array<Skill>;
+	title: string;
+	description: string;
+	image: string;
+	link?: string;
+	githubLink?: string;
+	playstoreLink?: string;
+	youtubeLink?: string;
+	devpostLink?: string;
+	madeWith: Skill[];
+	collaborators?: string[];
 }
 
 const ProjectItem: React.FC<Props> = (props) => {
-  return (
-    <div className={styles.container}>
-      <a
-        className={styles.imageAnchor}
-        href={props.link || props.playstoreLink}
-      >
-        <img className={styles.image} src={props.image} />
-      </a>
-      <div className={styles.text}>
-        <strong className={styles.title}>{props.title}</strong>
-        <p className={styles.description}>{props.description}</p>
-        <div className={styles.madeWithLabel}>Made with: </div>
-        <div className={styles.madeWith}>
-          {props.madeWith.map((skill, idx) => {
-            return <SkillPair skill={skill} key={idx} size="small" />;
-          })}
-        </div>
-      </div>
-      <div className={styles.links}>
-        {props.link && (
-          <a className={styles.link} href={props.link}>
-            Demo
-          </a>
-        )}
-        {props.githubLink && (
-          <a className={styles.link} href={props.githubLink}>
-            <img src="/img/github.svg" alt="github logo" width="40px" />
-          </a>
-        )}
-        {props.playstoreLink && (
-          <a className={styles.link} href={props.playstoreLink}>
-            <img src="/img/playstore.svg" alt="playstore logo" width="30px" />
-          </a>
-        )}
-        {props.devpostLink && (
-          <a className={styles.link} href={props.devpostLink}>
-            <img src="/img/devpost.svg" alt="devpost logo" width="35px" />
-          </a>
-        )}
-        {props.youtubeLink && (
-          <a className={styles.link} href={props.youtubeLink}>
-            <img src="/img/youtube.svg" alt="youtube logo" width="40px" />
-          </a>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<a
+				className={styles.imageAnchor}
+				href={props.link || props.playstoreLink}
+			>
+				<img className={styles.image} src={props.image} />
+			</a>
+			<div className={styles.text}>
+				<strong className={styles.title}>{props.title}</strong>
+				<p className={styles.description}>{props.description}</p>
+				<div className={styles.madeWithLabel}>Made with: </div>
+				<div className={styles.madeWith}>
+					{props.madeWith.map((skill, idx) => {
+						return <SkillPair skill={skill} key={idx} size="small" />;
+					})}
+				</div>
+				{props.collaborators && <div>
+					<div className={styles.collabLabel}>Collaborators: </div>
+					<ul className={styles.collabList}>
+						{props.collaborators.map((collaborator, idx) => {
+							return <li className={styles.collabItem}>{collaborator}</li>;
+						})}
+					</ul>
+				</div>}
+			</div>
+			<div className={styles.links}>
+				{props.link && (
+					<a className={styles.link} href={props.link}>
+						Demo
+					</a>
+				)}
+				{props.githubLink && (
+					<a className={styles.link} href={props.githubLink}>
+						<img src="/img/github.svg" alt="github logo" width="40px" />
+					</a>
+				)}
+				{props.playstoreLink && (
+					<a className={styles.link} href={props.playstoreLink}>
+						<img src="/img/playstore.svg" alt="playstore logo" width="30px" />
+					</a>
+				)}
+				{props.devpostLink && (
+					<a className={styles.link} href={props.devpostLink}>
+						<img src="/img/devpost.svg" alt="devpost logo" width="35px" />
+					</a>
+				)}
+				{props.youtubeLink && (
+					<a className={styles.link} href={props.youtubeLink}>
+						<img src="/img/youtube.svg" alt="youtube logo" width="40px" />
+					</a>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default ProjectItem;
