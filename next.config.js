eecfbd6eefc396e4220fd7212857f9595db1c6ea
AjayLiu <ajayliu2928@gmail.com>
2025-11-05 11:39:19 -0800

    remove next config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Note: The "target" property is deprecated in Next.js 13+
-  // but may be required as a workaround for compatibility with Netlify adapter
-  target: 'serverless',
-}
-
-module.exports = nextConfig
-
