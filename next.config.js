52f9510827b83d765362b8238f397c303551004f
AjayLiu <ajayliu2928@gmail.com>
2025-11-05 11:19:35 -0800

    remove target

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Note: The "target" property is deprecated in Next.js 13+
+  // Let Netlify plugin handle the deployment configuration
+  // Do not use output: 'export' as it conflicts with @netlify/plugin-nextjs
+}
+
+module.exports = nextConfig
+
