21de357c5a1f8875f220c9530623df676c4fd5d1
AjayLiu <ajayliu2928@gmail.com>
2025-11-05 13:18:15 -0800

    do export?

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Temporary workaround: removed output: 'export' to work with outdated plugin
-  // TODO: Remove @netlify/plugin-nextjs@3.9.2 from Netlify UI, then re-enable output: 'export'
-  // With static export, you don't need the plugin at all
+  output: 'export',
 }
 
 module.exports = nextConfig
